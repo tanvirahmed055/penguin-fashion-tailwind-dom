c8032a580d1c3a9e6740ad47457d6b6abc1c1920
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-08-17 01:10:02 +0600

    v2

--- js/script.js
+++ js/script.js
@@ -9,6 +9,13 @@ console.log(womenHeaders.innerText);
 
 //২. সেখানে একদম হেডার এ একটা buy now বাটন আছে। সেই বাটনে একটা ক্লিক হ্যান্ডলার যোগ করতে হবে? সেটা কি করতে পারবে? যদি পারো তাহলে সেই বাটনে ক্লিক করলে উপরে যেখানে BE THE PENGUIN এই কথাগুলো আছে সেখানে BE THE PENGUIN এর পরিবর্তে "I AM PENGUIN" কথাটা লিখে দিতে পারবে? 
 
+
+// const heading = document.getElementById('get-heading');
+// console.log(heading.innerText);
+
+
 document.getElementById('get-buy-btn').addEventListener('click', function () {
-    const
-})
+    const heading = document.getElementById('get-heading');
+    heading.innerText = "I AM PENGUIN";
+});
+
